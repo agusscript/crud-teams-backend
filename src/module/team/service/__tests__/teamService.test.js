cb01af15fb03bf64f2638c4d5bc600a2dddaa7dc
agusscript <agus.sanchez.240@gmail.com>
2023-12-07 16:40:00 -0300

    Add unit test to teamController, complete coverage in teamService test

--- src/module/team/service/__tests__/teamService.test.js
+++ src/module/team/service/__tests__/teamService.test.js
@@ -19,15 +19,23 @@ describe("TeamService", () => {
     expect(repositoryMock.getAll).toHaveBeenCalledTimes(1);
   });
 
+  test("Call getById method without id parameter should throw specific error", () => {
+    expect(async () => {
+      await service.getById();
+    }).rejects.toThrow(TeamIdNotDefinedError);
+    expect(repositoryMock.getById).not.toHaveBeenCalled();
+  });
+
   test("Call getById method from repository one time", () => {
     service.getById(1);
     expect(repositoryMock.getById).toHaveBeenCalledTimes(1);
   });
 
-  test("Call getById method without id parameter should throw specific error", () => {
+  test("Call create method without team parameter should throw specific error", () => {
     expect(async () => {
-      await service.getById();
-    }).rejects.toThrow(TeamIdNotDefinedError);
+      await service.create();
+    }).rejects.toThrow(TeamNotDefinedException);
+    expect(repositoryMock.create).not.toHaveBeenCalled();
   });
 
   test("Call create method from repository to save a new team one time", () => {
@@ -35,10 +43,18 @@ describe("TeamService", () => {
     expect(repositoryMock.create).toHaveBeenCalledTimes(1);
   });
 
-  test("Call create method without team parameter should throw specific error", () => {
+  test("Call update method without id parameter should throw specific error", () => {
     expect(async () => {
-      await service.create();
+      await service.update(undefined, { name: "new team" });
+    }).rejects.toThrow(TeamIdNotDefinedError);
+    expect(repositoryMock.update).not.toHaveBeenCalled();
+  });
+
+  test("Call update method without team parameter should throw specific error", () => {
+    expect(async () => {
+      await service.update(1);
     }).rejects.toThrow(TeamNotDefinedException);
+    expect(repositoryMock.update).not.toHaveBeenCalled();
   });
 
   test("Call update method from repository to edit and save a team one time", () => {
@@ -46,20 +62,15 @@ describe("TeamService", () => {
     expect(repositoryMock.update).toHaveBeenCalledTimes(1);
   });
 
-  test("Call update method without team parameter should throw specific error", () => {
+  test("Call delete method without id parameter should throw specific error", () => {
     expect(async () => {
-      await service.update(1);
-    }).rejects.toThrow(TeamNotDefinedException);
+      await service.delete();
+    }).rejects.toThrow(TeamIdNotDefinedError);
+    expect(repositoryMock.delete).not.toHaveBeenCalled();
   });
 
   test("Call delete method to remove a team one time", () => {
     service.delete(1);
     expect(repositoryMock.delete).toHaveBeenCalledTimes(1);
-  });
-
-  test("Call delete method without id parameter should throw specific error", () => {
-    expect(async () => {
-      await service.delete();
-    }).rejects.toThrow(TeamIdNotDefinedError);
   });
 });
